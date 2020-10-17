97bdf3615f03e1c95ff20de8d70241280df14a2c
Jared Lim <jaredlim1@gmail.com>
2020-10-16 21:12:15 -0600

    added js to check date and time and update accordingly

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 $(document).ready(function(){
 
 // Set Date At Top of Screen
-var currentDay = $("#currentDay");
-currentDay = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentDay);
+var currentDate;
+
+function setDate() {
+    var dateCheck = localStorage.getItem("currentDate");
+    currentDate = moment().format("dddd, MMMM Do YYYY");
+    if (dateCheck === "" || dateCheck === currentDate) {
+        $("#currentDay").text(currentDate);
+        localStorage.setItem("currentDate", currentDate);
+    } else {
+        localStorage.clear();
+        $("#currentDay").text(currentDate);
+        localStorage.setItem("currentDate", currentDate);
+    };
+};
+
+setDate();
 
 
 // Set Local Storage when Save is Clicked
@@ -12,25 +25,28 @@ $("button.saveBtn").on("click", function() {
     var timeText = $(this).siblings("textarea.description").val();
     var timeID = $(this).siblings("textarea.description").attr("id");
     localStorage.setItem(timeID, timeText);
+    currentDate = moment().subtract(1, "days"); // HERE
 });
 
 
-// Set Time Text from Local Storage
+// Set Schedule Text from Local Storage
 for (var i = 8; i < 23; i++) {
     var timeText = localStorage.getItem([i]);
     $("#" + [i]).val(timeText);
 }
 
-// Set Time Text Background Based on Current Time
+// Set Text Background Based on Current Time
 var time = moment().format('HH');
-console.log(time);
+// console.log(time);
 
 function setColors() {
     for (var j = 8; j < 23; j++) {
+
     var ID = ($("#"+[j]).attr("id"))
     ID = Number(ID);
+    $("#" + [j]).removeClass("past present future")
+    // console.log(ID);
 
-    console.log(ID);
     if (ID < time) {
         $("#" + [j]).addClass("past");
     } else if (ID > time) {
@@ -38,16 +54,18 @@ function setColors() {
     } else if (ID = time) {
         $("#" + [j]).addClass("present");
     } else {
-        console.log("huh?");
+        console.log("Not Working");
     }
   }
 }
 
 setColors();
 
-// Update Colors every minute
+// Update Info Every 30 Seconds
 setInterval(function() {
+    time = moment().format('HH');
+    setDate();
     setColors();
-}, 60 * 1000); 
+}, 30 * 1000); 
 
 });
